ea8f3d26b8c6c7dc15e0b6f791e1cb5f31c337c3
Jeffery <parkjon8@gmail.com>
2022-02-08 04:29:31 +0000

    Refactor product specs code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,74 +2,42 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    before do
+      @category = Category.new(name: 'Apparel')
+      @product = Product.new(name: 'Hipster Hat', price: 34.49, quantity: 4, category: @category)
+    end
+
     it 'should save product with all four fileds set' do
-      @category = Category.create(name: 'Apparel')
-      
-      @product = Product.new
-      @product.name = 'Hipster Hat'
-      @product.price = 34.49
-      @product.quantity = 4
-      @product.category_id = @category.id
       @product.save
-      
-      expect(@product.valid?).to be true
+      expect(@product).to be_present
     end
 
     it 'should not save product if name field is not provided' do
-      @category = Category.create(name: 'Apparel')
-      # @category.save
-      
-      @product = Product.new
-      @product.name = nil
-      @product.price = 34.49
-      @product.quantity = 4
-      @product.category_id = @category.id
+      @product.name = nil      
       @product.save
       
-      expect(@product.valid?).to be false
-      expect(@product.errors.full_messages.count).to be 1
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'should not save product if price field is not provided' do
-      @category = Category.create(name: 'Apparel')
-      
-      @product = Product.new
-      @product.name = 'Hipster Hat'
-      @product.price = nil
-      @product.quantity = 4
-      @product.category_id = @category.id
+      @product.price_cents = nil
       @product.save
       
-      expect(@product.valid?).to be false
-      expect(@product.errors.full_messages.count).to be 3
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'should not save product if quantity field is not provided' do
-      @category = Category.create(name: 'Apparel')
-      
-      @product = Product.new
-      @product.name = 'Hipster Hat'
-      @product.price = 34.49
       @product.quantity = nil
-      @product.category_id = @category.id
       @product.save
       
-      expect(@product.valid?).to be false
-      expect(@product.errors.full_messages.count).to be 1
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'should not save product if category field is not provided' do
-      @category = Category.create(name: 'Apparel')
-      
-      @product = Product.new
-      @product.name = 'Hipster Hat'
-      @product.price = 34.49
-      @product.quantity = nil
-      @product.category_id = nil
+      @product.category = nil
       @product.save
       
-      expect(@product.valid?).to be false
-      expect(@product.errors.full_messages.count).to be 2
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
     
   end
